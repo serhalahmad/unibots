8e7cba7cdc043c3fb6ae7b0d15946525ab53a091
Karl Johannes <karl.frjo@gmail.com>
2025-03-27 13:22:02 +0000

    updated test_servo_motors to work with new servo motor scripts

--- test_components/test_servo_motors.py
+++ test_components/test_servo_motors.py
@@ -1,19 +1,29 @@
 import serial
+import time
 
 class Motor:
     def __init__(self, name, ser):
         self.name = name
         self.ser = ser
+        # Initialize motor hardware here
 
     def setVelocity(self, velocity):
+        # Send PWM signal or command to motor driver
         print(f"Setting {self.name} motors velocity to {velocity}")
+        # Example: "setVelocity 40 75" (left motor 40, right motor 75)
         command = str(self.name) + " " + str(velocity)
         self.ser.write(command.encode())
 
 ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 
-left_motor = Motor('left', ser)
-right_motor = Motor('right', ser)
+wheel_motors = Motor('setVelocity', ser)
 
-left_motor.setVelocity(75)
-right_motor.setVelocity(-75)
+wheel_motors.setVelocity(40, 40)
+time.sleep(1.5)
+wheel_motors.setVelocity(40, 0)
+time.sleep(1.5)
+wheel_motors.setVelocity(0, 40)
+time.sleep(1.5)
+wheel_motors.setVelocity(40, 40)
+time.sleep(3)
+wheel_motors.setVelocity(0, 0)
