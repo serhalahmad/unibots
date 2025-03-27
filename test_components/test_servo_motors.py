8e25fd0eba0445dc467675a10e95887cfc602e6f
unibots <ahmadserhal.289t@hotmail.com>
2025-03-27 14:42:04 +0000

    Test for motors and serial

--- test_components/test_servo_motors.py
+++ test_components/test_servo_motors.py
@@ -7,23 +7,27 @@ class Motor:
         self.ser = ser
         # Initialize motor hardware here
 
-    def setVelocity(self, velocity):
+    def setVelocity(self, left_velocity, right_velocity):
         # Send PWM signal or command to motor driver
-        print(f"Setting {self.name} motors velocity to {velocity}")
+        print(f"{self.name} for left wheels to {left_velocity} and right wheels to {right_velocity}")
         # Example: "setVelocity 40 75" (left motor 40, right motor 75)
-        command = str(self.name) + " " + str(velocity)
+        command = str(self.name) + " " + str(left_velocity) + " " + str(right_velocity) + '\n'
+        print(command)
         self.ser.write(command.encode())
 
 ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 
 wheel_motors = Motor('setVelocity', ser)
 
+ser.write("setVelocity 0 0\n".encode())
+time.sleep(2)
+
 wheel_motors.setVelocity(40, 40)
-time.sleep(1.5)
+time.sleep(5)
 wheel_motors.setVelocity(40, 0)
-time.sleep(1.5)
+time.sleep(5)
 wheel_motors.setVelocity(0, 40)
-time.sleep(1.5)
+time.sleep(5)
 wheel_motors.setVelocity(40, 40)
-time.sleep(3)
+time.sleep(5)
 wheel_motors.setVelocity(0, 0)
